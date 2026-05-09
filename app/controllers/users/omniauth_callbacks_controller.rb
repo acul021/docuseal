1341ee95a99708607773b91290296e76450ef8e7
acul021 <tucci.bimmelt@gmail.com>
2026-05-09 12:22:17 +0200

    Map SSO user role from configurable OIDC claim
    Adds an optional role_attribute on the SSO config so identity providers
    can drive Docuseal user roles (admin, editor/writer, viewer). The role
    is applied on provisioning and synced on each subsequent login. The
    recognized claim values are documented inline on the SSO settings page.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -18,7 +18,13 @@ module Users
       identity = UserOauthIdentity.find_by(provider: auth.provider, uid: auth.uid)
       user     = identity&.user || User.find_by(email: email, account: account)
 
-      user ||= provision_sso_user(account, email, auth)
+      mapped_role = role_from_claims(auth, config)
+
+      if user
+        user.update(role: mapped_role) if mapped_role && user.role != mapped_role
+      else
+        user = provision_sso_user(account, email, auth, mapped_role)
+      end
 
       unless user.active_for_authentication?
         return redirect_to new_user_session_path, alert: I18n.t('your_account_is_locked')
@@ -59,7 +65,7 @@ module Users
       EncryptedConfig.find_by(account: account, key: EncryptedConfig::OAUTH_CONFIGS_KEY)&.value
     end
 
-    def provision_sso_user(account, email, auth)
+    def provision_sso_user(account, email, auth, mapped_role = nil)
       first_name = auth.info.first_name.presence || auth.info.name.to_s.split(' ', 2).first
       last_name  = auth.info.last_name.presence  || auth.info.name.to_s.split(' ', 2).last
 
@@ -67,10 +73,29 @@ module Users
         email: email,
         first_name: first_name,
         last_name: last_name,
-        role: User::VIEWER_ROLE,
+        role: mapped_role || User::VIEWER_ROLE,
         password: SecureRandom.hex(32),
         confirmed_at: Time.current
       )
+    end
+
+    ROLE_CLAIM_MAP = {
+      'admin'  => User::ADMIN_ROLE,
+      'editor' => User::EDITOR_ROLE,
+      'writer' => User::EDITOR_ROLE,
+      'viewer' => User::VIEWER_ROLE
+    }.freeze
+
+    def role_from_claims(auth, config)
+      attribute = config&.dig('role_attribute').to_s.strip
+      return nil if attribute.blank?
+
+      raw_info = auth.extra&.raw_info.to_h
+      value = raw_info[attribute] || auth.info[attribute]
+      return nil if value.blank?
+
+      values = Array(value).flat_map { |v| v.to_s.downcase.split(/[\s,]+/) }
+      values.lazy.filter_map { |v| ROLE_CLAIM_MAP[v] }.first
     end
 
     def email_domain_allowed?(email, config)
